e7ea4cc341af5f1121297652fabbbde6479ed39e
technetbytes <saqibullah@msn.com>
2024-11-21 20:24:37 +0500

    EC2 Creation

--- AWS/EC2-Setup/main.tf
+++ AWS/EC2-Setup/main.tf
@@ -1,10 +1,10 @@
 # Configure the AWS provider
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-east-1"
 }
 
 # Create an EC2 instance
-resource "aws_instance" "ec2test" {
-  ami           = "ami-097c4e1feeea169e5"
+resource "aws_instance" "ec2test2" {
+  ami           = "ami-012967cc5a8c9f891"
   instance_type = "t2.micro"
 }
